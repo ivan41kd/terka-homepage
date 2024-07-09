b12346451468d5cdeb9bb440d51c638bd8d8454d
ivan41kd <grkorolevskiy@gmail.com>
2024-07-09 13:18:29 +0300

    fixed: logo width, padding for copyright, number

--- src/app/components/ui/Footer/Footer.tsx
+++ src/app/components/ui/Footer/Footer.tsx
@@ -5,8 +5,8 @@ import Image from 'next/image';
 export const Footer = () => {
   return (
     <footer className='footer'>
-      <div className='container'>
-        <div className='footer__main-wrapper'>
+      <div className='footer__main-wrapper'>
+        <div className='container'>
           <div className='footer__wrapper'>
             <div className='footer__left'>
               <div className='footer__text'>
@@ -46,13 +46,13 @@ export const Footer = () => {
               </div>
             </div>
           </div>
-          <div className='footer__bottom-wrapper'>
-            <p className='footer__copyright'>(С) Тёрка, 2020-2024</p>
-            <a href='tel:+79520559846'>
-              <p className='footer__number'>+7 952 055 98 46 </p>
-            </a>
-          </div>
         </div>
+      </div>
+      <div className='footer__bottom-wrapper'>
+        <p className='footer__copyright'>(С) Тёрка, 2020-2024</p>
+        <a href='tel:+79520559846'>
+          <p className='footer__number'>+7 952 055 98 46 </p>
+        </a>
       </div>
     </footer>
   );
